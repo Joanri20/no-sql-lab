61cb5226036ae7aa838f6c9ad1a16f586dd00007
Joanri20 <joanri20@hotmail.com>
2021-05-01 14:56:10 -0500

    Agregar los endpoints y agregar apellido

--- routes/authors.js
+++ routes/authors.js
@@ -17,6 +17,39 @@ router.get('/', async (req, res) => {
   }
 });
 
+//Consultar los nombres y los apellidos de los autores que tengan publicaciones inferiores o iguales a 20 y 
+// su país sea Colombia.
+router.get('/Consulta1', async (req, res) => {
+  try {
+   const authors = await Author.find({$and: [{"publicados":{$lte:20}},{"pais":"Colombia"}]},
+   {nombre:1,apellido:1,_id:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//Consultar los nombres de los autores que tengan apellido en la base de datos.
+router.get('/Consulta2', async (req, res) => {
+  try {
+    const authors = await Author.find({"apellido":{$exists:true}},{nombre:1,_id:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//Consultar los apellidos de los autores que tengan más de 20 publicaciones o que su país sea Argentina.
+router.get('/Consulta3', async (req, res) => {
+  try {
+    const authors = await Author.find({$or: [{"publicados":{$gt:20}},{"pais":"Argentina"}]},{apellido:1,_id:0});
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
 /**
  * Create a new Author
  */
